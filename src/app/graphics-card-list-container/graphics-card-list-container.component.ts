0b972d25813d8b9ae7f0e4fb9871080aa02ce145
Nico <>
2021-08-06 16:02:14 +0200

    Se crea el listado de gráficas paginado, se dispacha el loadGraphics y se hace el select al store

--- src/app/graphics-card-list-container/graphics-card-list-container.component.ts
+++ src/app/graphics-card-list-container/graphics-card-list-container.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { GraphicCard } from '../core/models/graphic-card.model';
+import { loadGraphicsCardList } from '../store/graphics-card-list/actions/graphics-card-list.actions';
 
 @Component({
   selector: 'app-graphics-card-list-container',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphics-card-list-container.component.scss']
 })
 export class GraphicsCardListContainerComponent implements OnInit {
+  config: any;
+  graphicsCardList$: Observable<GraphicCard[]>;
 
-  constructor() { }
+  constructor(private store: Store<{ graphicsCardList: GraphicCard[] }>) {}
 
   ngOnInit() {
+    this.config = {
+      itemsPerPage: 4,
+      currentPage: 1
+    };
+
+    this.store.dispatch(loadGraphicsCardList());
+    this.graphicsCardList$ = this.store
+      .select('graphicsCardList')
+      .pipe(tap((graphicsCardList) => (this.config.totalItems = graphicsCardList?.length)));
   }
 
+  pageChanged(event: number) {
+    this.config.currentPage = event;
+  }
 }
