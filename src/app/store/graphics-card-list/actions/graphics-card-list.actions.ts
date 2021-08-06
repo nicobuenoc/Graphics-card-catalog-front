354b29f8586755403e059f83a0ed70660e2412ed
Nico <>
2021-08-06 15:59:36 +0200

    Correción, la acciónde load GraphicsCardList no llega el número de página

--- src/app/store/graphics-card-list/actions/graphics-card-list.actions.ts
+++ src/app/store/graphics-card-list/actions/graphics-card-list.actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { GraphicCard } from 'src/app/core/models/graphic-card.model';
 
-export const loadGraphicsCardList = createAction('[Ships] Load GraphicsCardList', props<{ page?: number }>());
+export const loadGraphicsCardList = createAction('[Ships] Load GraphicsCardList');
 
 export const setGraphicsCardList  = createAction('[Ships] Set GraphicsCardList', props<{ graphicsCardList: GraphicCard[] }>());
 
